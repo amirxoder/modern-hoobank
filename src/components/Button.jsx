a29f1e1f39d40c1379d8c40fbae1b62ce4731a54
amirxoder <amirxoder@gmail.com>
2022-08-21 01:44:15 +0430

    complete billing cardDeal testimonail

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles, text }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} `}
+      className={`py-4 px-6 bg-blue-gradient rounded-[10px] font-poppins font-medium text-[18px] text-primary outline-none ${styles} `}
     >
       {text}
     </button>
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div
+        className={`${styles.flexCenter} flex-wrap 
+        w-full sm:min-w-[192px] min-w-[120px]`}
+      >
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter}`}>
+            <img
+              src={client.logo}
+              alt="logo"
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
- 
